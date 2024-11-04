62222eb01020b562c6ea9530daa2105000fdebe4
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2024-11-04 09:59:23 +0100

    Update uxsmd.js

--- cofounder/api/system/functions/pm/uxsmd.js
+++ cofounder/api/system/functions/pm/uxsmd.js
@@ -143,7 +143,7 @@ you're a genius`,
 				},
 			},
 			data: {
-				model: `chatgpt-4o-latest`, //`gpt-4o`,
+				model: `chatgpt-4o-mini`, //`gpt-4o`,
 				messages,
 				preparser: `backticks`,
 				parser: false,
